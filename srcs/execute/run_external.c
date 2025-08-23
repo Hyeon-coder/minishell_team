718ebf0ab8f5251b8b69cdec089077991d6f61b9
juhyeonl <juhyeonl@student.hive.fi>
2025-08-23 05:44:52 +0300

    120 FLAG HERE

--- srcs/execute/run_external.c
+++ srcs/execute/run_external.c
@@ -6,11 +6,47 @@
 /*   By: juhyeonl <juhyeonl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:04:18 by juhyeonl          #+#    #+#             */
-/*   Updated: 2025/08/23 03:19:57 by juhyeonl         ###   ########.fr       */
+/*   Updated: 2025/08/23 05:43:08 by juhyeonl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+/* 파일 상태를 확인하고 적절한 오류 메시지와 exit 코드를 결정 */
+static int	check_file_executable(const char *path, char **error_msg)
+{
+	struct stat	st;
+
+	if (stat(path, &st) != 0)
+	{
+		if (access(path, F_OK) == 0)
+		{
+			*error_msg = ": Permission denied\n";
+			return (126);
+		}
+		else
+		{
+			*error_msg = ": No such file or directory\n";
+			return (127);
+		}
+	}
+	
+	if (S_ISDIR(st.st_mode))
+	{
+		*error_msg = ": Is a directory\n";
+		return (126);
+	}
+	
+	if (access(path, X_OK) != 0)
+	{
+		*error_msg = ": Permission denied\n";
+		return (126);
+	}
+	
+	/* 실행 가능하지만 execve가 실패한 경우 */
+	*error_msg = ": command not found\n";
+	return (127);
+}
 
 /* Build a NULL-terminated "KEY=VAL" array from env list */
 static char	**env_list_to_envp(t_env *env)
@@ -70,7 +106,6 @@ static char	*get_path_value(t_env *env_list)
 {
 	t_env	*node;
 
-	// 기존 find_env 대신 env_find 사용 (일관성)
 	node = env_find(env_list, "PATH");
 	if (node)
 		return (node->value);
@@ -129,27 +164,40 @@ void	run_external(char **argv, t_env *env_list, t_shell *sh)
 	char	**envp;
 	char	**paths;
 	char	*pathvar;
+	char	*error_msg;
+	int		exit_code;
 
 	(void)sh;
 	if (!argv || !argv[0] || argv[0][0] == '\0')
 		return ;
+	
 	envp = env_list_to_envp(env_list);
 	if (!envp)
 	{
 		perror("minishell: malloc");
 		return ;
 	}
+	
 	if (has_slash(argv[0]))
 	{
+		/* 직접 경로인 경우: 파일 상태를 먼저 확인 */
+		exit_code = check_file_executable(argv[0], &error_msg);
 		try_exec(argv[0], argv, envp);
-		err_with_cmd(NULL, (char *)argv[0], ": command not found\n");
+		
+		/* execve 실패 - 적절한 오류 메시지와 exit 코드 */
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd((char *)argv[0], 2);
+		ft_putstr_fd(error_msg, 2);
 		ft_free_2d_array(envp);
-		return ;
+		exit(exit_code);
 	}
+	
+	/* PATH에서 찾는 경우 */
 	pathvar = get_path_value(env_list);
 	paths = split_paths(pathvar);
 	if (try_exec_in_paths(argv, paths, envp) < 0)
 		err_with_cmd(NULL, (char *)argv[0], ": command not found\n");
 	ft_free_2d_array(envp);
 	ft_free_2d_array(paths);
+	exit(127); /* PATH에서 찾을 수 없으면 127 */
 }
